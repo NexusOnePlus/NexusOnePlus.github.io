6f56a52020b78e1a551eb6cb0712eb1dbfdef9d3
Nexus <wv35788@gmail.com>
2024-03-30 19:58:30 -0500

    First commit

---
+++ ostias.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([4.639386, -74.082412],6);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
